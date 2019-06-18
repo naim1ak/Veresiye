80c910532abdb9b738190f48ebfd800a63ffdeac
naim ak <irecak@hotmail.com>
2019-06-18 15:03:31 +0300

    Ana Form Yapımı

--- Veresiye.UI/FrmMain.cs
+++ Veresiye.UI/FrmMain.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Veresiye.Service;
 
 namespace Veresiye.UI
 {
     public partial class FrmMain : Form
     {
+        private readonly ICompanyService companyService;
+
         public FrmMain()
         {
+        }
+
+        public FrmMain(ICompanyService companyService)
+        {
+            this.companyService = companyService;
             InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(companyService.GetAll().Count().ToString());
         }
     }
 }
