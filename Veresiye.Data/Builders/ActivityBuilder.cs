b5ad2a79527cb2f6edb474af909e4f944dca896f
naim ak <irecak@hotmail.com>
2019-06-19 10:06:15 +0300

    Fluent_api ekleniyor

---
+++ Veresiye.Data/Builders/ActivityBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veresiye.Model;
+
+namespace Veresiye.Data.Builders
+{
+    public class ActivityBuilder
+    {
+        public ActivityBuilder(EntityTypeConfiguration<Activity> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Name).IsRequired().HasMaxLength(100);
+            
+        }
+
+    }
+}
